fc611d276f5fc350ffcd56350f0afbadc2f3cc23
mikehollas123 <hollas@gmail.com>
2021-09-19 14:41:16 -0500

    rebuilt added google account auth

--- HollasBlog/Program.cs
+++ HollasBlog/Program.cs
@@ -1,5 +1,4 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -9,7 +8,7 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace blogApp
+namespace HollasBlog
 {
     public class Program
     {
@@ -20,8 +19,10 @@ namespace blogApp
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-            builder.Services.AddOidcAuthentication(options => 
+            builder.Services.AddOidcAuthentication(options =>
             {
+                // Configure your authentication provider options here.
+                // For more information, see https://aka.ms/blazor-standalone-auth
                 builder.Configuration.Bind("google", options.ProviderOptions);
             });
 
